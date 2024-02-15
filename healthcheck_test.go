1e5089118f5298211e61c1ecfdcfc4d69ea3f510
Keloran <keloran@develbox.info>
2024-02-15 21:32:47 +0000

    fix: replace deprecated ioutil.ReadAll with io.ReadAll in healthcheck test
    The change was made to update the deprecated ioutil.ReadAll function with the recommended io.ReadAll function in the healthcheck test. This ensures that the codebase stays up to date with best practices and avoids potential issues with deprecated functions in the future.

--- healthcheck_test.go
+++ healthcheck_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -90,7 +90,7 @@ func TestHTTP(t *testing.T) {
 		healthcheck.HTTP(response, request)
 		assert.Equal(t, 200, response.Code)
 
-		body, _ := ioutil.ReadAll(response.Body)
+		body, _ := io.ReadAll(response.Body)
 		healthy := healthcheck.Health{}
 		_ = json.Unmarshal(body, &healthy)
 		assert.Equal(t, test.expect, healthy)
